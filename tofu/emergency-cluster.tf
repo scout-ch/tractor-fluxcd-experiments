f223ad7a771db06bb7397f24e3579e3d06bf43c5
Nicolas Da Mutten <nicolas.damutten@pbs.ch>
2026-03-16 21:06:32 +0100

    fix: add emergency cluster (#15)

---
+++ tofu/emergency-cluster.tf
@@ -0,0 +1,52 @@
+module "kaas_emergency" {
+  source = "./modules/kaas"
+
+  infomaniak_public_cloud = {
+    id         = "2976"
+    project_id = "34898"
+  }
+
+  cluster = {
+    name      = "tractor-k8s-emergency"
+    version   = "1.35"
+    pack_name = "dedicated_4"
+    region    = "dc3-a"
+  }
+
+  cluster_instance_pools = {
+    default = {
+      min_instances     = 2
+      max_instances     = 2
+      availability_zone = "dc3-a-10"
+    }
+  }
+}
+
+module "flux_kaas_emergency" {
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/flux"
+
+  cluster_name         = "tractor-k8s-emergency"
+  github_repository    = "tractor-k8s-emergency-config"
+  webhook_ingress_host = "webhook.emergency.k8s.tractor.scout.ch"
+
+  providers = {
+    flux       = flux.emergency-cluster
+    kubernetes = kubernetes.emergency-cluster
+  }
+}
+
+module "traefik_kaas_emergency" {
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/traefik"
+
+  cluster_config_repository = module.flux_kaas_emergency.config_repository
+  cluster_config_path       = module.flux_kaas_emergency.cluster_config_path
+  load_balancer_ip          = "195.15.192.244"
+}
+
+module "cert_manager_kaas_emergency" {
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/cert_manager"
+
+  cluster_config_repository = module.flux_kaas_emergency.config_repository
+  cluster_config_path       = module.flux_kaas_emergency.cluster_config_path
+}
+
--- tofu/tenants.tf
+++ tofu/tenants.tf
@@ -6,3 +6,13 @@ module "t_whoami" {
   tenant_name               = "whoami"
   webhook_ingress_host      = local.webhook_host
 }
+
+module "ns_pbs_who_is_who_kaas_emergency" {
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/namespace"
+
+  name = "pbs-who-is-who"
+
+  providers = {
+    kubernetes = kubernetes.emergency-cluster
+  }
+}
