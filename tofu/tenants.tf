8b4bdf9c24d329fda9c9d6c7b8922df5747d3c4f
Nicolas Da Mutten <nicolas@damutten.ch>
2026-03-16 21:28:58 +0100

    fix: rename emergency namespace

--- tofu/tenants.tf
+++ tofu/tenants.tf
@@ -7,10 +7,10 @@ module "t_whoami" {
   webhook_ingress_host      = local.webhook_host
 }
 
-module "ns_pbs_who_is_who_kaas_emergency" {
+module "ns_pbs_pfadi_finder_midata_adapter_kaas_emergency" {
   source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/namespace"
 
-  name = "pbs-who-is-who"
+  name = "pbs-pfadi-finder-midata-adapter"
 
   providers = {
     kubernetes = kubernetes.emergency-cluster
