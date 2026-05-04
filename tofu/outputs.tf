ddf6a6db87a7e684ba6c7f3e4355cc326a2ec0e7
Nicolas Da Mutten <nicolas@damutten.ch>
2026-05-04 22:59:52 +0200

    fix: add clever emergency cluster user output

--- tofu/outputs.tf
+++ tofu/outputs.tf
@@ -2,3 +2,8 @@ output "kubeconfig_clever" {
   value     = module.u_clever.kubeconfig
   sensitive = true
 }
+
+output "kubeconfig_clever_kaas_emergency" {
+  value     = module.u_clever_kaas_emergency.kubeconfig
+  sensitive = true
+}
