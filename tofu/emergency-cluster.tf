b5d85fb6f23644171c69b4472167a3f1de42c46f
Nicolas Da Mutten <nicolas@damutten.ch>
2026-05-07 22:48:44 +0200

    chore(deps): update all non-major dependencies

--- tofu/emergency-cluster.tf
+++ tofu/emergency-cluster.tf
@@ -23,7 +23,7 @@ module "kaas_emergency" {
 }
 
 module "flux_kaas_emergency" {
-  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/flux?ref=7aff942248ee59f793bbbea7b2ce176bb2df77c6" # main
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/flux?ref=8b065151b20c8f636c1b140e614255020484ad33" # main
 
   cluster_name         = "tractor-k8s-emergency"
   github_repository    = "tractor-k8s-emergency-config"
@@ -36,7 +36,7 @@ module "flux_kaas_emergency" {
 }
 
 module "traefik_kaas_emergency" {
-  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/traefik?ref=7aff942248ee59f793bbbea7b2ce176bb2df77c6" # main
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/traefik?ref=8b065151b20c8f636c1b140e614255020484ad33" # main
 
   cluster_config_repository = module.flux_kaas_emergency.config_repository
   cluster_config_path       = module.flux_kaas_emergency.cluster_config_path
@@ -44,9 +44,8 @@ module "traefik_kaas_emergency" {
 }
 
 module "cert_manager_kaas_emergency" {
-  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/cert_manager?ref=7aff942248ee59f793bbbea7b2ce176bb2df77c6" # main
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/cert_manager?ref=8b065151b20c8f636c1b140e614255020484ad33" # main
 
   cluster_config_repository = module.flux_kaas_emergency.config_repository
   cluster_config_path       = module.flux_kaas_emergency.cluster_config_path
 }
-
--- tofu/tenants.tf
+++ tofu/tenants.tf
@@ -1,6 +1,6 @@
 
 module "t_whoami" {
-  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/tenant?ref=7aff942248ee59f793bbbea7b2ce176bb2df77c6" # main
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/tenant?ref=8b065151b20c8f636c1b140e614255020484ad33" # main
 
   cluster_config_repository = module.flux.config_repository
   tenant_name               = "whoami"
@@ -8,7 +8,7 @@ module "t_whoami" {
 }
 
 module "ns_pbs_pfadi_finder_midata_adapter_kaas_emergency" {
-  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/namespace?ref=7aff942248ee59f793bbbea7b2ce176bb2df77c6" # main
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/namespace?ref=8b065151b20c8f636c1b140e614255020484ad33" # main
 
   name = "pbs-pfadi-finder-midata-adapter"
 
