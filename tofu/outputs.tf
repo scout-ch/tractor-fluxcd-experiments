38e136d5955287a8ccf12ea082ad8b17cecfc38b
Nicolas Da Mutten <nicolas@damutten.ch>
2026-09-12 19:10:57 +0200

    feat: add nano and floh to emergency cluster

--- tofu/outputs.tf
+++ tofu/outputs.tf
@@ -7,3 +7,13 @@ output "kubeconfig_clever_kaas_emergency" {
   value     = module.u_clever_kaas_emergency.kubeconfig
   sensitive = true
 }
+
+output "kubeconfig_nano_kaas_emergency" {
+  value     = module.u_nano_kaas_emergency.kubeconfig
+  sensitive = true
+}
+
+output "kubeconfig_floh_kaas_emergency" {
+  value     = module.u_floh_kaas_emergency.kubeconfig
+  sensitive = true
+}
