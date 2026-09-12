bfe30be8740754bf4f58e0e48d788f68beb4bf5a
Nicolas Da Mutten <nicolas@damutten.ch>
2026-09-12 19:10:57 +0200

    feat: add nano and floh to emergency cluster

--- tofu/users.tf
+++ tofu/users.tf
@@ -19,3 +19,29 @@ module "u_clever_kaas_emergency" {
     kubernetes = kubernetes.emergency-cluster
   }
 }
+
+module "u_nano_kaas_emergency" {
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/user?ref=70ec554132e5006fc940024249d8f515796ff8cc" # main
+
+  username          = "nano"
+  default_namespace = null
+
+  cluster_info = module.kaas_emergency.cluster_info
+
+  providers = {
+    kubernetes = kubernetes.emergency-cluster
+  }
+}
+
+module "u_floh_kaas_emergency" {
+  source = "git::ssh://git@github.com/scout-ch/tractor-k8s-tenants.git//tofu/modules/user?ref=70ec554132e5006fc940024249d8f515796ff8cc" # main
+
+  username          = "floh"
+  default_namespace = null
+
+  cluster_info = module.kaas_emergency.cluster_info
+
+  providers = {
+    kubernetes = kubernetes.emergency-cluster
+  }
+}
